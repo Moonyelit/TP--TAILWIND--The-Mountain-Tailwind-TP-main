72149d83c53ca21261de232c188ae085083b3da9
moonye@live.fr <moonye@live.fr>
2024-11-04 11:52:00 +0100

    ADD : Début header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          cyan: "#22d3ee" ,
+          red: "#e11d48",
+        },
+        neutral: {
+          "off-white": "hsl(36, 100%, 99%)",
+          "dark-grayish-blue": "hsl(236, 13%, 42%)",
+          "grayish-blue": "hsl(233, 8%, 79%)",
+          "very-dark-blue": "hsl(240, 100%, 5%)",
+        },
+      },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        paragraph: "15px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
